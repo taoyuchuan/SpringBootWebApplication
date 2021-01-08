c2dfe1e0be26e210e4baf9847d631b83b8f63570
taoyuchuan <taoyuchuan@gmail.com>
2021-01-07 22:02:25 -0500

    Add scheduler

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -2,11 +2,13 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * Driver class.
  */
 @SpringBootApplication
+@EnableScheduling
 public class Application {
 
 	/**
