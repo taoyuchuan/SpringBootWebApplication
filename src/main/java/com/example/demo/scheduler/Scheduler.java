578b33fe38d46b0aa7fc088634292fecb7d86971
taoyuchuan <taoyuchuan@gmail.com>
2021-01-07 22:25:29 -0500

    Enable https

--- src/main/java/com/example/demo/scheduler/Scheduler.java
+++ src/main/java/com/example/demo/scheduler/Scheduler.java
@@ -5,8 +5,15 @@ import java.util.Date;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+/**
+ * Scheduler class.
+ */
 @Component
 public class Scheduler {
+    /**
+     * Cron scheduler.
+     * Execute the task every minute starting at 9:00 AM and ending at 9:59 AM, every day.
+     */
     @Scheduled(cron = "0 * 9 * * ?")
     public void cronJobSch() {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
@@ -15,6 +22,10 @@ public class Scheduler {
         System.out.println("Java cron job expression:: " + strDate);
     }
 
+    /**
+     * Fixed rate schedule.
+     * Value in milliseconds.
+     */
     @Scheduled(fixedRate = 1000)
     public void fixedRateSch() {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
@@ -24,6 +35,10 @@ public class Scheduler {
         System.out.println("Fixed Rate scheduler:: " + strDate);
     }
 
+    /**
+     * Fixed delay scheduler.
+     * Value in milliseconds.
+     */
     @Scheduled(fixedDelay = 1000, initialDelay = 3000)
     public void fixedDelaySch() {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
